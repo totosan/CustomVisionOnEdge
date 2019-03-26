43d0a98a3853ae2c61336b7e23fedcaeda726edf
To To <toto_san@live.com>
2019-03-26 22:40:30 +0100

    copy only, if probability >0.4

--- getResults.ps1
+++ getResults.ps1
@@ -1,6 +1,6 @@
 $picspath = "C:\Users\ttomow\OneDrive\Bilder\alerts2\video\RQGBG\grabs"
 
-$pics = Get-ChildItem -Path $picspath -Filter "*.jpg" | Select-Object -Skip 1000 -First 2000
+$pics = Get-ChildItem -Path $picspath -Filter "*.jpg" | Select-Object -Skip 2000 
 Write-Host "$($pics.Length) images found"
 
 New-Item "R:\Temp\post" -ItemType Directory
@@ -8,8 +8,10 @@ New-Item "R:\Temp\post" -ItemType Directory
 foreach ($item in $pics) {
     $result = Invoke-RestMethod -Method Post -Uri http://127.0.0.1:8081/image -ContentType "application/octet-stream" -InFile $item.FullName
     if ($result.predictions.tagName -eq "Postauto" ) {
-        $auto = $result.predictions | %{if($_.tagName -eq "Postauto") { $_.probability}} 
-        Write-Host "$($item.FullName) -> $auto %"
-        Copy-Item -Path $item.FullName -Destination "r:\temp\post\$($item.Name)"
+        $auto = $result.predictions | % {if ($_.tagName -eq "Postauto") { $_.probability}} 
+        if ($auto -gt 0.4) {
+            Write-Host "$($item.FullName) -> $auto %"
+            Copy-Item -Path $item.FullName -Destination "r:\temp\post\$($item.Name)"
+        }
     }
 }
