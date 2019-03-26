ab7f325603d810a74a09f4eb58397026413217d3
To To <toto_san@live.com>
2019-03-26 22:28:55 +0100

    first running version

---
+++ getResults.ps1
@@ -0,0 +1,15 @@
+$picspath = "C:\Users\ttomow\OneDrive\Bilder\alerts2\video\RQGBG\grabs"
+
+$pics = Get-ChildItem -Path $picspath -Filter "*.jpg" | Select-Object -Skip 1000 -First 2000
+Write-Host "$($pics.Length) images found"
+
+New-Item "R:\Temp\post" -ItemType Directory
+
+foreach ($item in $pics) {
+    $result = Invoke-RestMethod -Method Post -Uri http://127.0.0.1:8081/image -ContentType "application/octet-stream" -InFile $item.FullName
+    if ($result.predictions.tagName -eq "Postauto" ) {
+        $auto = $result.predictions | %{if($_.tagName -eq "Postauto") { $_.probability}} 
+        Write-Host "$($item.FullName) -> $auto %"
+        Copy-Item -Path $item.FullName -Destination "r:\temp\post\$($item.Name)"
+    }
+}
