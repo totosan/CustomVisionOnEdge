70bbd4324ae44ec917bd2cfb6ab948205a9511ae
To To <toto_san@live.com>
2019-03-30 23:02:44 +0100

    detecting objects in playing video - finished

--- VisionAI_WPF/VisionAI_WPF/MainWindow.xaml.cs
+++ VisionAI_WPF/VisionAI_WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -9,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace VisionAI_WPF
 {
@@ -16,71 +21,106 @@ namespace VisionAI_WPF
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-	{
+    {
         HttpClient _client = new HttpClient();
+        private Uri _uri;
+        private int _vidHeight;
+        private int _vidWidth;
+        private Size _dpi;
 
         public MainWindow()
-		{
-			InitializeComponent();
-			this.Loaded += MainWindow_Loaded;
-		}
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
 
-	
 
-		private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-		{
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
             DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Interval = TimeSpan.FromSeconds(0.6d);
             timer.Tick += GetFrameAsync;
             timer.Start();
 
-            _client.BaseAddress = new Uri("http://127.0.0.1:8081/image");
+            var path = ConfigurationManager.AppSettings["path"];
+            var uri = ConfigurationManager.AppSettings["uri"];
+            _uri = new Uri(uri);
 
-            VideoPlayer.Source = new Uri(@"C:\Users\toto_\OneDrive\Bilder\alerts2\video\RQGBG\1_2018-09-23_07-58-23.mp4", UriKind.Absolute);
-			VideoPlayer.LoadedBehavior = MediaState.Manual;
-			VideoPlayer.Play();
 
-		}
+            VideoPlayer.Source = new Uri(path, UriKind.Absolute);
+            VideoPlayer.LoadedBehavior = MediaState.Manual;
+            VideoPlayer.MediaOpened += (o, args) =>
+            {
+                _vidHeight = VideoPlayer.NaturalVideoHeight;
+                _vidWidth = VideoPlayer.NaturalVideoWidth;
+                _dpi = new Size(96, 96);
+
+            };
+            VideoPlayer.Play();
+
+
+        }
 
         private async Task<string> UploadImage(RenderTargetBitmap bmp)
         {
-            MemoryStream stream = new MemoryStream();
-            BitmapEncoder encoder = new BmpBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(bmp));
-            encoder.Save(stream);
-
-            MultipartFormDataContent form = new MultipartFormDataContent();
-            HttpContent content = new StringContent("fileToUpload");
-            form.Add(content, "fileToUpload");
-            
-            content = new StreamContent(stream);
-            content.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+            using (MemoryStream stream = new MemoryStream())
             {
-                Name = "fileToUpload",
-                FileName = "whatever"
-            };
-            form.Add(content);
-            var response = await _client.PostAsync("", form);
-            return response.Content.ReadAsStringAsync().Result;
+                BitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(bmp));
+                encoder.Save(stream);
+
+                var byteData = stream.ToArray();
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                    var response = await _client.PostAsync(_uri, content);
+                    return response.Content.ReadAsStringAsync().Result;
+                }
+            }
         }
 
         async void GetFrameAsync(object sender, EventArgs e)
-		{
-			Size dpi = new Size(96, 96);
-			RenderTargetBitmap bmp =
-			  new RenderTargetBitmap(300, 200,
-				dpi.Width, dpi.Height, PixelFormats.Pbgra32);
-			bmp.Render(VideoPlayer);
+        {
+            if (!VideoPlayer.HasVideo)
+                return;
+
+            RenderTargetBitmap bmp =
+              new RenderTargetBitmap(_vidWidth, _vidHeight,
+                _dpi.Width, _dpi.Height, PixelFormats.Pbgra32);
+            bmp.Render(VideoPlayer);
+
+            painting.Children?.Clear();
 
             var result = await UploadImage(bmp);
+            var predictions = PredictionResult.FromJson(result);
+            var relevant = predictions.Predictions.Where(x => x.Probability > 0.5);
 
-            var rect = new Rectangle();
-			rect.Stroke = new SolidColorBrush(Color.FromRgb(100,0,0));
-			rect.StrokeThickness = 5;
-			rect.Height = 100;
-			rect.Width = 100;
-			rect.Margin = new Thickness(100,10,0,0);
-			painting.Children.Add(rect);
-		}
-	}
-} 
+            int i = 0;
+            foreach (var prediction in relevant)
+            {
+                var boundingBox = new Rect(
+                    prediction.BoundingBox.Left,
+                    prediction.BoundingBox.Top,
+                    prediction.BoundingBox.Width,
+                    prediction.BoundingBox.Height);
+
+                var rect = new Rectangle();
+                rect.Stroke = new SolidColorBrush(Color.FromRgb(100, (byte)(i * 10), 0));
+                rect.StrokeThickness = 1;
+                rect.Height = boundingBox.Height * _vidHeight;
+                rect.Width = boundingBox.Width * _vidWidth;
+                rect.Margin = new Thickness(boundingBox.Left * _vidWidth, boundingBox.Top * _vidHeight, 0, 0);
+
+                var text = new TextBlock();
+                text.Text = prediction.TagName;
+                text.Margin = rect.Margin;
+                text.Background = new SolidColorBrush(Color.FromRgb(100, (byte)(i * 10), 0));
+                text.Foreground = new SolidColorBrush(Color.FromRgb(250, 250, 250));
+                painting.Children.Add(text);
+                painting.Children.Add(rect);
+            }
+        }
+    }
+}
